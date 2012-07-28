4bfc35bab6c0869ef343783f376307e618a7b072
Roman Efimov <romaonthego@gmail.com>
2012-07-27 21:04:16 -0500

    Initial Commit

---
+++ REPhotoCollectionControllerExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  REPhotoCollectionControllerExample
+//
+//  Created by Roman Efimov on 7/27/12.
+//  Copyright (c) 2012 Roman Efimov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
